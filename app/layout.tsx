1bda7e41b2bd3c7bff362a068de0700b34243eb2
Micha Vie <micha@vleap.io>
2024-09-04 12:44:42 +0200

    enforce light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className="bg-white lg:bg-zinc-100 dark:bg-zinc-900 dark:lg:bg-zinc-950"
+      className="light bg-white lg:bg-zinc-100 dark:bg-zinc-900 dark:lg:bg-zinc-950"
     >
       <body className={inter.className}>
         <BaseLayout>{children}</BaseLayout>
