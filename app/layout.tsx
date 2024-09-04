46b45d91b722bac0dfa9ebadd5f007e6f087e05d
Micha Vie <micha@vleap.io>
2024-09-04 11:18:48 +0200

    fix layouts dir name casing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { BaseLayout } from './layouts_temp/BaseLayout'
+import { BaseLayout } from './layouts/BaseLayout'
 
 const inter = Inter({ subsets: ['latin'] })
 
