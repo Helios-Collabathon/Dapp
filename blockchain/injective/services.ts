c34fc9edce15dd8f2befb98959d951333e0f99e6
Davide Marino <marinov170697@gmail.com>
2024-09-11 19:01:02 +0200

    env fix

--- blockchain/injective/services.ts
+++ blockchain/injective/services.ts
@@ -1,15 +1,17 @@
-import { ChainGrpcBankApi, ChainGrpcWasmApi } from '@injectivelabs/sdk-ts'
-import { Network, getNetworkEndpoints } from '@injectivelabs/networks'
-import { MsgBroadcaster } from '@injectivelabs/wallet-ts'
-import { walletStrategy } from './wallet'
+import { ChainGrpcBankApi, ChainGrpcWasmApi } from "@injectivelabs/sdk-ts";
+import { Network, getNetworkEndpoints } from "@injectivelabs/networks";
+import { MsgBroadcaster } from "@injectivelabs/wallet-ts";
+import { walletStrategy } from "./wallet";
 
-export const NETWORK = Network.TestnetSentry
-export const ENDPOINTS = getNetworkEndpoints(NETWORK)
+export const NETWORK = process.env.NEXT_PUBLIC_NETWORK
+  ? Network.MainnetSentry
+  : Network.TestnetSentry;
+export const ENDPOINTS = getNetworkEndpoints(NETWORK);
 
-export const WasmAPI = new ChainGrpcWasmApi(ENDPOINTS.grpc)
-export const BankAPI = new ChainGrpcBankApi(ENDPOINTS.grpc)
+export const WasmAPI = new ChainGrpcWasmApi(ENDPOINTS.grpc);
+export const BankAPI = new ChainGrpcBankApi(ENDPOINTS.grpc);
 
 export const BroadcastClient = new MsgBroadcaster({
   walletStrategy,
   network: NETWORK,
-})
+});
