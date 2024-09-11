dc7af78ec6fe3e2c33a7ab40299138b92206f1a8
Davide Marino <marinov170697@gmail.com>
2024-09-11 15:02:01 +0200

    style changes

--- app/features/layout/Navigation.tsx
+++ app/features/layout/Navigation.tsx
@@ -1,82 +1,106 @@
-'use client'
-import { Config } from '@/app/config'
-import { WalletContext } from '@/blockchain/wallet-provider'
-import { faMoon, faRightFromBracket, faSun } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import { useContext, useEffect, useState } from 'react'
-import LogoText from '../../images/logo-text.png'
-import { Field } from '../controls/Fieldset'
-import { Input } from '../controls/Input'
-import { Navbar, NavbarItem, NavbarSection, NavbarSpacer } from '../controls/Navbar'
-import LoginDropDown from '../loginButton/LoginDropDown'
-import { logout } from '@multiversx/sdk-dapp/utils'
+"use client";
+import { Config } from "@/app/config";
+import { WalletContext } from "@/blockchain/wallet-provider";
+import {
+  faMoon,
+  faRightFromBracket,
+  faSun,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useContext, useEffect, useState } from "react";
+import LogoText from "../../images/logo-text.png";
+import { Field } from "../controls/Fieldset";
+import { Input } from "../controls/Input";
+import {
+  Navbar,
+  NavbarItem,
+  NavbarSection,
+  NavbarSpacer,
+} from "../controls/Navbar";
+import LoginDropDown from "../loginButton/LoginDropDown";
+import { logout } from "@multiversx/sdk-dapp/utils";
 
-type Props = {}
+type Props = {};
 
 export function Navigation(props: Props) {
-  const currentPath = usePathname()
-  const [searchQuery, setSearchQuery] = useState('')
-  const { connectedWallet } = useContext(WalletContext)
+  const currentPath = usePathname();
+  const [searchQuery, setSearchQuery] = useState("");
+  const { connectedWallet } = useContext(WalletContext);
 
   const [isDarkMode, setIsDarkMode] = useState(() => {
-    if (typeof window !== 'undefined') {
-      const storedTheme = localStorage.getItem('theme')
+    if (typeof window !== "undefined") {
+      const storedTheme = localStorage.getItem("theme");
       // Default to dark mode if no theme is set in localStorage
       if (storedTheme) {
-        return storedTheme === 'dark'
+        return storedTheme === "dark";
       } else {
-        return true
+        return true;
       }
     }
-    return false
-  })
+    return false;
+  });
 
   useEffect(() => {
     if (isDarkMode) {
-      document.documentElement.classList.add('dark')
-      localStorage.setItem('theme', 'dark')
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
     } else {
-      document.documentElement.classList.remove('dark')
-      localStorage.setItem('theme', 'light')
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
     }
-  }, [isDarkMode])
+  }, [isDarkMode]);
 
   const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode)
-  }
+    setIsDarkMode(!isDarkMode);
+  };
 
-  const isCurrentPage = (path: string) => currentPath === path
+  const isCurrentPage = (path: string) => currentPath === path;
 
   const onSearch = () => {
     if (searchQuery.trim()) {
-      window.location.href = `${window.location.origin}/persona/${searchQuery.trim()}`
+      window.location.href = `${window.location.origin}/persona/${searchQuery.trim()}`;
     }
-  }
+  };
 
   const onKeyDownHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-      onSearch()
+    if (event.key === "Enter") {
+      onSearch();
     }
-  }
+  };
 
   return (
     <Navbar className="flex flex-wrap items-center p-4 sm:flex-nowrap sm:justify-between">
       <div className="hidden items-center gap-4 sm:flex">
         <Link href="/" aria-label="Home">
-          <Image src={LogoText} alt="Logo" priority className="mr-4 w-full" style={{ height: '50px' }} />
+          <Image
+            src={LogoText}
+            alt="Logo"
+            priority
+            className="mr-4 w-full"
+            style={{ height: "50px" }}
+          />
         </Link>
         <NavbarSection className="hidden md:flex">
-          <NavbarItem href={Config.Pages.Start} current={isCurrentPage(Config.Pages.Start)}>
+          <NavbarItem
+            href={Config.Pages.Start}
+            current={isCurrentPage(Config.Pages.Start)}
+          >
             Start
           </NavbarItem>
-          <NavbarItem href={Config.Pages.Faq} current={isCurrentPage(Config.Pages.Faq)}>
+          <NavbarItem
+            href={Config.Pages.Faq}
+            current={isCurrentPage(Config.Pages.Faq)}
+          >
             FAQ
           </NavbarItem>
           {connectedWallet.address && (
-            <NavbarItem href={Config.Pages.Persona} current={isCurrentPage(Config.Pages.Persona)}>
+            <NavbarItem
+              href={Config.Pages.Persona}
+              current={isCurrentPage(Config.Pages.Persona)}
+            >
               My Persona
             </NavbarItem>
           )}
@@ -93,24 +117,29 @@ export function Navigation(props: Props) {
       </Field>
       <NavbarSpacer className="hidden sm:flex" />
       <div className="hidden items-center gap-3 sm:flex">
-        <FontAwesomeIcon onClick={toggleTheme} icon={isDarkMode ? faSun : faMoon} cursor={'pointer'} color={isDarkMode ? 'white' : 'black'} />
+        <FontAwesomeIcon
+          onClick={toggleTheme}
+          icon={isDarkMode ? faSun : faMoon}
+          cursor={"pointer"}
+          color={isDarkMode ? "white" : "black"}
+        />
         <LoginDropDown />
         {connectedWallet.address && (
           <FontAwesomeIcon
-            className="ml-4 cursor-pointer"
+            className="cursor-pointer"
             onClick={() => {
-              if (connectedWallet.provider === 'webwallet') {
-                logout(Config.Pages.Start, undefined, false)
-                localStorage.removeItem('wallet-provider')
+              if (connectedWallet.provider === "webwallet") {
+                logout(Config.Pages.Start, undefined, false);
+                localStorage.removeItem("wallet-provider");
               }
-              localStorage.removeItem('connected-wallet')
-              window.location.href = `${window.location.origin}`
+              localStorage.removeItem("connected-wallet");
+              window.location.href = `${window.location.origin}`;
             }}
-            color={isDarkMode ? 'white' : 'black'}
+            color={"red"}
             icon={faRightFromBracket}
           />
         )}
       </div>
     </Navbar>
-  )
+  );
 }
--- blockchain/wallet-provider.tsx
+++ blockchain/wallet-provider.tsx
@@ -1,95 +1,123 @@
-'use client'
-import dynamic from 'next/dynamic'
-import { createContext, useContext, useEffect, useState } from 'react'
-import { Config } from '../app/config'
-import { Chain, ConnectedWallet } from './types/connected-wallet'
-import { NotificationModal } from '@multiversx/sdk-dapp/UI'
+"use client";
+import dynamic from "next/dynamic";
+import { createContext, useContext, useEffect, useState } from "react";
+import { Config } from "../app/config";
+import { Chain, ConnectedWallet } from "./types/connected-wallet";
+import { NotificationModal } from "@multiversx/sdk-dapp/UI";
 
-type AppWalletProvider = string
+type AppWalletProvider = string;
 
 type ContextType = {
-  connectWallet: (address: string, chain: Chain, provider: AppWalletProvider) => Promise<void>
-  disconnectWallet: () => void
-  connectedWallet: ConnectedWallet
-}
+  connectWallet: (
+    address: string,
+    chain: Chain,
+    provider: AppWalletProvider,
+  ) => Promise<void>;
+  disconnectWallet: () => void;
+  connectedWallet: ConnectedWallet;
+};
 
-const WalletStorageKey = 'connected-wallet'
+const WalletStorageKey = "connected-wallet";
 
-const DappProvider = dynamic(async () => (await import('@multiversx/sdk-dapp/wrappers/DappProvider')).DappProvider, { ssr: false })
+const DappProvider = dynamic(
+  async () =>
+    (await import("@multiversx/sdk-dapp/wrappers/DappProvider")).DappProvider,
+  { ssr: false },
+);
 
 const SignTransactionsModals = dynamic(
-  async () => (await import('@multiversx/sdk-dapp/UI/SignTransactionsModals/SignTransactionsModals')).SignTransactionsModals,
-  { ssr: false }
-)
+  async () =>
+    (
+      await import(
+        "@multiversx/sdk-dapp/UI/SignTransactionsModals/SignTransactionsModals"
+      )
+    ).SignTransactionsModals,
+  { ssr: false },
+);
 
-const TransactionsToastList = dynamic(async () => (await import('@multiversx/sdk-dapp/UI/TransactionsToastList')).TransactionsToastList, { ssr: false })
+const TransactionsToastList = dynamic(
+  async () =>
+    (await import("@multiversx/sdk-dapp/UI/TransactionsToastList"))
+      .TransactionsToastList,
+  { ssr: false },
+);
 // const NotificationModal = dynamic(async () => (await import('@multiversx/sdk-dapp/UI/NotificationModal')).NotificationModal, { ssr: false })
 
 export const WalletContext = createContext<ContextType>({
   connectWallet: async () => {},
   disconnectWallet: () => {},
   connectedWallet: {
-    address: '',
+    address: "",
     chain: Chain.Injective,
-    provider: '',
+    provider: "",
   },
-})
+});
 
-export const useWallet = () => useContext(WalletContext)
+export const useWallet = () => useContext(WalletContext);
 
-export const WalletContextProvider = ({ children }: { children: React.ReactNode }) => {
-  const env = Config.App.Env === 'local' ? 'devnet' : Config.App.Env
+export const WalletContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const env = Config.App.Env === "local" ? "devnet" : Config.App.Env;
 
   const [connectedWallet, setConnectedWallet] = useState<ConnectedWallet>({
-    address: '',
+    address: "",
     chain: Chain.Injective,
-    provider: '',
-  })
+    provider: "",
+  });
 
   useEffect(() => {
     try {
-      const storedWallet = localStorage.getItem(WalletStorageKey)
+      const storedWallet = localStorage.getItem(WalletStorageKey);
       if (storedWallet) {
-        setConnectedWallet(JSON.parse(storedWallet))
+        setConnectedWallet(JSON.parse(storedWallet));
       }
     } catch (error) {
-      console.error('Failed to parse stored wallet from localStorage', error)
+      console.error("Failed to parse stored wallet from localStorage", error);
     }
-  }, [])
+  }, []);
 
-  const connectWallet = async (address: string, chain: Chain, provider: AppWalletProvider) => {
-    const wallet: ConnectedWallet = { address, chain, provider }
-    setConnectedWallet(wallet)
-    localStorage.setItem(WalletStorageKey, JSON.stringify(wallet))
-  }
+  const connectWallet = async (
+    address: string,
+    chain: Chain,
+    provider: AppWalletProvider,
+  ) => {
+    const wallet: ConnectedWallet = { address, chain, provider };
+    setConnectedWallet(wallet);
+    localStorage.setItem(WalletStorageKey, JSON.stringify(wallet));
+  };
 
   const disconnectWallet = () => {
-    setConnectedWallet({ address: '', provider: '', chain: Chain.Injective })
-    localStorage.removeItem(WalletStorageKey)
-  }
+    setConnectedWallet({ address: "", provider: "", chain: Chain.Injective });
+    localStorage.removeItem(WalletStorageKey);
+  };
 
   return (
-    <WalletContext.Provider value={{ connectedWallet, connectWallet, disconnectWallet }}>
+    <WalletContext.Provider
+      value={{ connectedWallet, connectWallet, disconnectWallet }}
+    >
       <DappProvider
         environment={env}
         customNetworkConfig={{
-          name: 'customConfig',
+          name: "customConfig",
           apiTimeout: 30_000,
           walletConnectV2ProjectId: Config.Services.WalletConnect.ProjectId,
           metamaskSnapWalletAddress: Config.Services.MetamaskSnap(),
         }}
         dappConfig={{
           shouldUseWebViewProvider: true,
-          isSSR: typeof window === 'undefined', // SSR detection
+          isSSR: typeof window === "undefined", // SSR detection
         }}
       >
         {children}
-        <TransactionsToastList />
+        <TransactionsToastList className="bg-red-500 text-green-500" />
         <NotificationModal />
-        <SignTransactionsModals />
+        <SignTransactionsModals className="model-card" />
       </DappProvider>
     </WalletContext.Provider>
-  )
-}
+  );
+};
 
-export default WalletContextProvider
+export default WalletContextProvider;
